cbea21d075a5c0e42e4e12779e27d3736a782d97
Piercingxx <155384428+Piercingxx@users.noreply.github.com>
2025-09-30 20:14:20 -0400

    Update fonts.sh

--- scripts/fonts.sh
+++ scripts/fonts.sh
@@ -15,15 +15,16 @@ builddir=$(pwd)
             mkdir -p /home/"$username"/.icons
         fi
         chown -R "$username":"$username" /home/"$username"/.icons
-    # Installing fonts
+            # Installing fonts
+        echo "Installing Fonts"
         cd "$builddir" || exit
         wget https://github.com/ryanoasis/nerd-fonts/releases/download/v2.1.0/FiraCode.zip
         wget https://github.com/ryanoasis/nerd-fonts/releases/download/v2.1.0/Meslo.zip
-        wget http://www.i18nguy.com/unicode/andagii.zip
+        wget https://github.com/ryanoasis/nerd-fonts/releases/latest/download/NerdFontsSymbolsOnly.zip
         unzip FiraCode.zip -d /home/"$username"/.fonts
         unzip Meslo.zip -d /home/"$username"/.fonts
-        unzip andagii.zip -d /home/"$username"/.fonts
-        sudo rm FiraCode.zip Meslo.zip andagii.zip
+        unzip NerdFontsSymbolsOnly.zip -d /home/"$username"/.fonts
+        sudo rm FiraCode.zip Meslo.zip NerdFontsSymbolsOnly.zip
         dnf install fontawesome-fonts -y 
         dnf install google-noto-emoji-color-fonts -y
         sudo yum install curl cabextract xorg-x11-font-utils fontconfig -y
@@ -31,3 +32,4 @@ builddir=$(pwd)
     # Reloading Font
         fc-cache -vf
         wait
+
